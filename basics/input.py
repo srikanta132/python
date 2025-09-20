6c93236ccd91a686c713c291b4d5086ca8c6a928
Srikanta Barik <107643164+srikanta132@users.noreply.github.com>
2025-09-20 23:55:07 +0530

    Add files via upload

---
+++ basics/input.py
@@ -0,0 +1,2 @@
+x = int(input('Enter Data :'))
+print(x)
---
+++ basics/literals.py
@@ -0,0 +1,14 @@
+x = 123_34_56
+print(x)
+
+name = '"python"'
+print(name)
+
+y = hex(45)
+print(y)
+
+y = oct(45)
+print(y)
+
+y = bin(45)
+print(y)
