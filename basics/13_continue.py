f2b92458c4ac50d625c6edfa07f04264ff1abf08
Srikanta Barik <107643164+srikanta132@users.noreply.github.com>
2025-09-26 21:45:27 +0530

    Add files via upload

---
+++ basics/13_continue.py
@@ -0,0 +1,9 @@
+x = 10
+while x>=1:
+    if x==5 or x==3:
+        x = x-1
+        continue
+    print(x)
+    x = x-1
+    
+    
---
+++ basics/14_range.py
@@ -0,0 +1,8 @@
+for i in range(10):
+    print(i)
+
+# working with string
+s = 'python'
+for i in s:
+    print(i)
+    
