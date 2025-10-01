8586be83678e289cf8b551472710601ae42bcbe4
Srikanta Barik <107643164+srikanta132@users.noreply.github.com>
2025-10-01 10:24:58 +0530

    Add files via upload

---
+++ 01-String/03_slice.py
@@ -0,0 +1,9 @@
+name = 'hello_world'
+
+nm = name[::3]
+
+print(nm)
+print(name[-1:-8:-3])
+
+s = 'sradha'
+print(str.upper(s))
---
+++ 01-String/04_find().py
@@ -0,0 +1,9 @@
+qoutes = 'andha hoke pyar nhi karna chahiye'
+index = qoutes.find('pyar')
+print(index)
+
+nm = 'if you get them'
+print(nm.find('hi'))
+
+s = 'no means no'
+print(s.find('o',3))
