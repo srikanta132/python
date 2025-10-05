77aafa6336df48ab16eacbb13372456402108d7a
Srikanta Barik <107643164+srikanta132@users.noreply.github.com>
2025-10-05 10:22:45 +0530

    Add files via upload

---
+++ 01-String/10-strip().py
@@ -0,0 +1,25 @@
+# lsrip(chars)
+text = '********hello'
+s = text.lstrip('*')
+text_1 = '          hello'
+s1 = text_1.lstrip()
+print(s)
+print(s1)
+
+# rsrip(chars)
+
+txt = 'hello**********'
+s1 = txt.rstrip('*')
+txt_1 = 'hello           '
+s2 = txt_1.rstrip()
+print(s1)
+print(s2)
+
+#strip(chars)
+
+name = '       sradha          '
+nm = name.strip()
+print(nm)
+
+tx = '#hello  *'
+print(tx.strip())
---
+++ 01-String/11-replace().py
@@ -0,0 +1,7 @@
+text = 'h-e-l-l-o'
+s1 = text.replace('-',',',3)
+print(s1)                   # Output h,e,l,l-o
+
+nm = 'srikanta barik'
+new_nm = nm.replace('srikanta','srikant')
+print(new_nm)               # Output srikant barik
